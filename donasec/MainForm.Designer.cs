568e8507e51b70ad180400a547b2f79d2edafced
Peyman Gohari <peyman.gohari@gmail.com>
2019-04-16 17:06:43 +1000

    Minor form text changes...

--- donasec/MainForm.Designer.cs
+++ donasec/MainForm.Designer.cs
@@ -40,6 +40,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.textBoxHelp = new System.Windows.Forms.TextBox();
+            this.textBoxHelp2 = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.groupBox4.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -58,7 +59,7 @@
             this.groupBox1.Size = new System.Drawing.Size(556, 171);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "What is this?";
+            this.groupBox1.Text = "\'d on a sec! Not so fast...";
             // 
             // groupBox4
             // 
@@ -135,20 +136,21 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(6, 21);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(462, 13);
+            this.label1.Size = new System.Drawing.Size(470, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "You have clicked on a web link (email, etc.). This will open up a web browser. Is" +
-    " it safe to do so?";
+    " it what you want?";
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.textBoxHelp2);
             this.groupBox2.Controls.Add(this.textBoxHelp);
             this.groupBox2.Location = new System.Drawing.Point(574, 12);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(214, 171);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "\'d on a sec...";
+            this.groupBox2.Text = "Tell me more...";
             // 
             // textBoxHelp
             // 
@@ -157,10 +159,21 @@
             this.textBoxHelp.Multiline = true;
             this.textBoxHelp.Name = "textBoxHelp";
             this.textBoxHelp.ReadOnly = true;
-            this.textBoxHelp.Size = new System.Drawing.Size(202, 146);
+            this.textBoxHelp.Size = new System.Drawing.Size(202, 60);
             this.textBoxHelp.TabIndex = 1;
             this.textBoxHelp.Text = "A weblink can get you anywhere, potentially installing software on your machine o" +
     "r trying to get sensitive information from you. Don\'t fall for it.";
+            // 
+            // textBoxHelp2
+            // 
+            this.textBoxHelp2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxHelp2.Location = new System.Drawing.Point(6, 87);
+            this.textBoxHelp2.Multiline = true;
+            this.textBoxHelp2.Name = "textBoxHelp2";
+            this.textBoxHelp2.ReadOnly = true;
+            this.textBoxHelp2.Size = new System.Drawing.Size(202, 27);
+            this.textBoxHelp2.TabIndex = 2;
+            this.textBoxHelp2.Text = "More info on www.donasec.com";
             // 
             // MainForm
             // 
@@ -198,6 +211,7 @@
         public System.Windows.Forms.TextBox textBoxLink;
         public System.Windows.Forms.ComboBox comboBoxBrowsers;
         public System.Windows.Forms.Button buttonAbort;
+        private System.Windows.Forms.TextBox textBoxHelp2;
     }
 }
 
