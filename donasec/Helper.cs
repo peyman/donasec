f965ce0358e75a493f150a79180e300cbecdbbc7
Peyman Gohari <peyman.gohari@gmail.com>
2019-04-16 14:48:03 +1000

    Setup and Run modes, with elevation of privileges

---
+++ donasec/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Principal;
+
+namespace donasec
+{
+    static class Helper
+    {
+        public static bool IsCurrentUserElevated()
+        {
+            WindowsIdentity userIdentity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal user = new WindowsPrincipal(userIdentity);
+            bool isUserAdmin = user.IsInRole(WindowsBuiltInRole.Administrator);
+            return isUserAdmin;
+        }
+
+
+    }
+}
--- donasec/Program.cs
+++ donasec/Program.cs
@@ -1,22 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace donasec
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            if (args.Length == 0)   // Setup mode
+            {
+                if (Helper.IsCurrentUserElevated())     // Admin privileges, proceed with Install
+                {
+                    ;
+                }
+                else    // Not admin, raise privileges
+                {
+                    Process.Start(
+                        new ProcessStartInfo()
+                        {
+                            FileName = System.Reflection.Assembly.GetExecutingAssembly().Location,
+                            UseShellExecute = true,
+                            Verb = "runas"
+                        }
+                    );
+                }
+            }
+            else   // Run mode - proceed if args[0] is a URI and discard the other args
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+            }
         }
     }
 }
